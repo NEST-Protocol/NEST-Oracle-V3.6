9a3a3ab5778e59fe378a92add40bc1d2287482da
chenf <baiynui@hotmail.com>
2021-04-04 13:42:35 +0800

    Develop to rinkeby

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,11 +4,11 @@ const IBNEST = artifacts.require("IBNEST");
 
 module.exports = function(deployer, network) {
   //deployer.deploy(Migrations);
-  console.log('network:' + network);
 
-  if (network == 'development') {
+  console.log('network:' + network);
+  if (network == 'development') 
+  {
     deployer.deploy(IterableMapping);
     deployer.link(IterableMapping, IBNEST);
   }
-  // deployer.deploy(IBNEST);
 };
