bfd1313e527a81c6b6d8e289861008aeb56a749a
Web Ninja <x7y8tt@gmail.com>
2021-05-14 16:17:12 +0800

    Add settings for BSC testnet & mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -31,6 +31,8 @@ const config = envResult.parsed;
 //console.log(config.RINKEBY_MNEMONIC, config.RINKEBY_NODEADDR);
 //console.log(config.ROPSTEN_MNEMONIC, config.ROPSTEN_NODEADDR);
 //console.log(config.MAINNET_MNEMONIC, config.MAINNET_NODEADDR);
+//console.log(config.BSCTESTNET_MNEMONIC, config.BSCTESTNET_NODEADDR);
+//console.log(config.BSC_MNEMONIC, config.BSC_NODEADDR);
 
 module.exports = {
   /**
@@ -116,6 +118,35 @@ module.exports = {
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    // Settings for Binance Smart Chain(BSC)
+    // See [Truffle - Binance Chain Docs](https://docs.binance.org/smart-chain/developer/deploy/truffle.html)
+    // bsctestnet: BSC Testnet
+    bsctestnet: {
+      // NB: It's important to wrap the provider as a function.
+      provider: () => new HDWalletProvider({
+        privateKeys: [config.BSCTESTNET_MNEMONIC],
+        providerOrUrl: config.BSCTESTNET_NODEADDR,
+      }),
+      //provider: () => new HDWalletProvider(mnemonic, 'https://data-seed-prebsc-1-s1.binance.org:8545'),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    // bsc: BSC Mainnet
+    bsc: {
+      // NB: It's important to wrap the provider as a function.
+      provider: () => new HDWalletProvider({
+        privateKeys: [config.BSC_MNEMONIC],
+        providerOrUrl: config.BSC_NODEADDR,
+      }),
+      //provider: () => new HDWalletProvider(mnemonic, 'https://bsc-dataseed1.binance.org'),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
 
     // Useful for private networks
